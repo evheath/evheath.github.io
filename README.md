7a8501fb8d1c4d5192287014df4ddd46431bbeee
evheath <elliot.v.heath@gmail.com>
2022-03-23 21:17:28 -0500

    redoing resume structure

---
+++ README.md
@@ -0,0 +1,10 @@
+# [Live](https://evheath.github.io/)
+
+
+## Development server
+
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+
+## Deployment
+
+Run `npm run deploy`
---
+++ src/app/languages.ts
@@ -0,0 +1,25 @@
+import { deviconInterface } from './devicon.interface'
+
+export const languages: deviconInterface[] = [
+  { devicon: "devicon-html5-plain", deviconWithText: "devicon-html5-plain-wordmark" },
+  { devicon: "devicon-css3-plain", deviconWithText: "devicon-css3-plain-wordmark" },
+  { devicon: "devicon-javascript-plain", deviconWithText: "devicon-javascript-plain" },
+  { devicon: "devicon-jquery-plain", deviconWithText: "devicon-jquery-plain-wordmark" },
+  { devicon: "devicon-angularjs-plain", deviconWithText: "devicon-angularjs-plain-wordmark" },
+  { devicon: "devicon-firebase-plain", deviconWithText: "devicon-firebase-plain-wordmark" },
+  { devicon: "devicon-typescript-plain", deviconWithText: "devicon-typescript-plain" },
+  { devicon: "devicon-nodejs-plain", deviconWithText: "devicon-nodejs-plain-wordmark" },
+  { devicon: "devicon-express-original", deviconWithText: "devicon-express-original-wordmark" },
+  { devicon: "devicon-ruby-plain", deviconWithText: "devicon-ruby-plain-wordmark" },
+  { devicon: "devicon-rails-plain", deviconWithText: "devicon-rails-plain-wordmark" },
+  { devicon: "devicon-postgresql-plain", deviconWithText: "devicon-postgresql-plain-wordmark" },
+  { devicon: "devicon-mongodb-plain", deviconWithText: "devicon-mongodb-plain-wordmark" },
+  { devicon: "devicon-mysql-plain", deviconWithText: "devicon-mysql-plain-wordmark" },
+  { devicon: "devicon-sqlite-plain", deviconWithText: "devicon-sqlite-plain-wordmark" },
+  { devicon: "devicon-vuejs-plain", deviconWithText: "devicon-vuejs-plain-wordmark" },
+  { devicon: "devicon-bootstrap-plain", deviconWithText: "devicon-bootstrap-plain-wordmark" },
+  { devicon: "devicon-python-plain", deviconWithText: "devicon-python-plain-wordmark" },
+  { devicon: "devicon-java-plain", deviconWithText: "devicon-java-plain-wordmark" },
+  { devicon: "devicon-django-plain", deviconWithText: "devicon-django-plain-wordmark" },
+  { devicon: "devicon-latex-original", deviconWithText: "devicon-latex-original-wordmark" },
+]
