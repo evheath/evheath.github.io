506d05b6b9bea89861eec1d16cd9b07a6137a6e3
evheath <elliot@businessfactory.dev>
2025-01-08 09:38:18 -0600

    names/titles for techs

--- src/app/devicon.interface.ts
+++ src/app/devicon.interface.ts
@@ -1,6 +1,7 @@
 export interface deviconInterface {
-  devicon: string // plain icon e.g. devicon-css3-plain-wordmark
-  deviconWithText: string // icon with text e.g. devicon-css3-plain-wordmark
-  hovering?: boolean // used to determine if user is hovering over the icon
-  clicked?: boolean // used to determine if user is hovering over the icon
+  devicon: string; // plain icon e.g. devicon-css3-plain-wordmark
+  deviconWithText: string; // icon with text e.g. devicon-css3-plain-wordmark
+  name: string; // name of the language
+  hovering?: boolean; // used to determine if user is hovering over the icon
+  clicked?: boolean; // used to determine if user is hovering over the icon
 }
--- src/app/languages.ts
+++ src/app/languages.ts
@@ -1,27 +1,100 @@
-import { deviconInterface } from './devicon.interface'
+import { deviconInterface } from "./devicon.interface";
 
 // https://devicon.dev/
 export const languages: deviconInterface[] = [
-  { devicon: "devicon-html5-plain", deviconWithText: "devicon-html5-plain-wordmark" },
-  { devicon: "devicon-css3-plain", deviconWithText: "devicon-css3-plain-wordmark" },
-  { devicon: "devicon-javascript-plain", deviconWithText: "devicon-javascript-plain" },
-  { devicon: "devicon-jquery-plain", deviconWithText: "devicon-jquery-plain-wordmark" },
-  { devicon: "devicon-angularjs-plain", deviconWithText: "devicon-angularjs-plain-wordmark" },
-  { devicon: "devicon-firebase-plain", deviconWithText: "devicon-firebase-plain-wordmark" },
-  { devicon: "devicon-typescript-plain", deviconWithText: "devicon-typescript-plain" },
-  { devicon: "devicon-nodejs-plain", deviconWithText: "devicon-nodejs-plain-wordmark" },
-  { devicon: "devicon-express-original", deviconWithText: "devicon-express-original-wordmark" },
-  { devicon: "devicon-ruby-plain", deviconWithText: "devicon-ruby-plain-wordmark" },
-  { devicon: "devicon-rails-plain", deviconWithText: "devicon-rails-plain-wordmark" },
-  { devicon: "devicon-postgresql-plain", deviconWithText: "devicon-postgresql-plain-wordmark" },
-  { devicon: "devicon-mongodb-plain", deviconWithText: "devicon-mongodb-plain-wordmark" },
-  { devicon: "devicon-mysql-plain", deviconWithText: "devicon-mysql-plain-wordmark" },
-  { devicon: "devicon-sqlite-plain", deviconWithText: "devicon-sqlite-plain-wordmark" },
-  { devicon: "devicon-vuejs-plain", deviconWithText: "devicon-vuejs-plain-wordmark" },
-  { devicon: "devicon-bootstrap-plain", deviconWithText: "devicon-bootstrap-plain-wordmark" },
-  { devicon: "devicon-tailwindcss-plain", deviconWithText: "devicon-tailwindcss-original-wordmark" },
-  { devicon: "devicon-python-plain", deviconWithText: "devicon-python-plain-wordmark" },
-  { devicon: "devicon-java-plain", deviconWithText: "devicon-java-plain-wordmark" },
-  // { devicon: "devicon-django-plain", deviconWithText: "devicon-django-plain-wordmark" },
-  { devicon: "devicon-latex-original", deviconWithText: "devicon-latex-original" },
-]
+  {
+    devicon: "devicon-html5-plain",
+    deviconWithText: "devicon-html5-plain-wordmark",
+    name: "HTML",
+  },
+  {
+    devicon: "devicon-css3-plain",
+    deviconWithText: "devicon-css3-plain-wordmark",
+    name: "CSS",
+  },
+  {
+    devicon: "devicon-nextjs-plain",
+    deviconWithText: "devicon-nextjs-original-wordmark",
+    name: "Next.js",
+  },
+  {
+    devicon: "devicon-javascript-plain",
+    deviconWithText: "devicon-javascript-plain",
+    name: "JavaScript",
+  },
+  {
+    devicon: "devicon-jquery-plain",
+    deviconWithText: "devicon-jquery-plain-wordmark",
+    name: "jQuery",
+  },
+  {
+    devicon: "devicon-angularjs-plain",
+    deviconWithText: "devicon-angularjs-plain-wordmark",
+    name: "Angular",
+  },
+  {
+    devicon: "devicon-firebase-plain",
+    deviconWithText: "devicon-firebase-plain-wordmark",
+    name: "Firebase",
+  },
+  {
+    devicon: "devicon-typescript-plain",
+    deviconWithText: "devicon-typescript-plain",
+    name: "TypeScript",
+  },
+  {
+    devicon: "devicon-nodejs-plain",
+    deviconWithText: "devicon-nodejs-plain-wordmark",
+    name: "Node.js",
+  },
+  {
+    devicon: "devicon-express-original",
+    deviconWithText: "devicon-express-original-wordmark",
+    name: "Express",
+  },
+  {
+    devicon: "devicon-ruby-plain",
+    deviconWithText: "devicon-ruby-plain-wordmark",
+    name: "Ruby",
+  },
+  {
+    devicon: "devicon-rails-plain",
+    deviconWithText: "devicon-rails-plain-wordmark",
+    name: "Rails",
+  },
+  {
+    devicon: "devicon-postgresql-plain",
+    deviconWithText: "devicon-postgresql-plain-wordmark",
+    name: "PostgreSQL",
+  },
+  {
+    devicon: "devicon-mongodb-plain",
+    deviconWithText: "devicon-mongodb-plain-wordmark",
+    name: "MongoDB",
+  },
+  {
+    devicon: "devicon-mysql-plain",
+    deviconWithText: "devicon-mysql-plain-wordmark",
+    name: "MySQL",
+  },
+  {
+    devicon: "devicon-sqlite-plain",
+    deviconWithText: "devicon-sqlite-plain-wordmark",
+    name: "SQLite",
+  },
+  {
+    devicon: "devicon-vuejs-plain",
+    deviconWithText: "devicon-vuejs-plain-wordmark",
+    name: "Vue.js",
+  },
+  {
+    devicon: "devicon-bootstrap-plain",
+    deviconWithText: "devicon-bootstrap-plain-wordmark",
+    name: "Bootstrap",
+  },
+  {
+    devicon: "devicon-tailwindcss-plain",
+    deviconWithText: "devicon-tailwindcss-original-wordmark",
+    name: "Tailwind CSS",
+  },
+];
